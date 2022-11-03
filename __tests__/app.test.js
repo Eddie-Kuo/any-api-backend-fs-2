3147018146fb8b343a0dc12ff8b1d1095bb8ce5a
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-11-03 18:00:48 -0500

    test 1 written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,28 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const { request } = require('express');
+const app = require('../lib/app');
 // const request = require('supertest');
 // const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('dog routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  test('/dogs should return list of dog names + id', async () => {
+    const res = await request(app).get('/dogs');
+    const expected = dogs.map((dog) => {
+      return { id: dog.id, name: dog.name };
+    });
+    expect(res.body).toEqual(expected);
   });
   afterAll(() => {
     pool.end();
   });
 });
+
+// describe('backend-express-template routes', () => {
+//   it('example test - delete me!', () => {
+//     expect(1).toEqual(1);
+//   });
+// });
