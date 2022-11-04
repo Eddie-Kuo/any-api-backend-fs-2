fc008559a48a91c00d8677d548a95680deab279c
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-11-03 20:29:44 -0500

    test 1 passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,8 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const { request } = require('express');
 const app = require('../lib/app');
-// const request = require('supertest');
+const request = require('supertest');
+const { dogs } = require('../lib/dogs-data');
 // const app = require('../lib/app');
 
 describe('dog routes', () => {
@@ -16,6 +16,13 @@ describe('dog routes', () => {
     });
     expect(res.body).toEqual(expected);
   });
+  // test('/dogs', async () => {
+  //   console.log(dogs);
+  //   const expected = await request(app).post('/dogs').send(dogs);
+  //   console.log(expected);
+  //   const res = await request(app).get('/dogs');
+  //   expect(res.body).toEqual(expected);
+  // });
   afterAll(() => {
     pool.end();
   });
@@ -26,3 +33,19 @@ describe('dog routes', () => {
 //     expect(1).toEqual(1);
 //   });
 // });
+
+// it('gets a cat by id', async () => {
+//   const cat = {
+//     name: 'Tilly',
+//     age: 2,
+//     favoriteTreat: 'canned food',
+//   };
+//   const returnedCat = await request(app).post('/cat/:id').send(cat);
+//* .post and .send to send the created object to your database to then compare what is shown in app
+//* const expected = await Cat.findById(returnedCat.id);
+//   const res = await request(app).get(`/cat/${returnedCat.body.id}`);
+
+//   expect(res.body).toEqual({ ...returnedCat.body });
+// });
+
+//! hard code data for the get all functions
